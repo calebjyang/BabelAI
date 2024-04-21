d09ac77fe9805fcda89ac326fa02a98979e6fca5
Caleb Yang <kb@jaymoo.com>
2024-04-20 19:55:37 -0700

    starting to implement "responses" screen

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class _MyHomePageState extends State<MyHomePage> {
   SpeechToText _speechToText = SpeechToText();
   bool _speechEnabled = false;
   String _lastWords = '';
+  bool _isListening = false;
 
   @override
   void initState() {
@@ -46,18 +47,33 @@ class _MyHomePageState extends State<MyHomePage> {
   /// Each time to start a speech recognition session
   void _startListening() async {
     print('Starting listening...');
-    await _speechToText.listen(onResult: _onSpeechResult);
-    setState(() {});
+    await _speechToText.listen(
+      onResult: _onSpeechResult,
+      localeId: 'es_ES', // Set the language to Spanish
+    );
+    setState(() {
+      _isListening = true;
+      _lastWords = ''; // Clear last words when starting listening
+    });
   }
 
   /// Manually stop the active speech recognition session
   /// Note that there are also timeouts that each platform enforces
-  /// and the SpeechToText plugin supports setting timeouts on the
-  /// listen method.
+  /// and the SpeechToText plugin supports setting timeouts on
+  /// the listen method.
   void _stopListening() async {
     print('Stopping listening...');
     await _speechToText.stop();
-    setState(() {});
+    setState(() {
+      _isListening = false;
+    });
+
+    // Navigate to the new page
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => ResultPage(lastWords: _lastWords)),
+    );
   }
 
   /// This is the callback that the SpeechToText plugin calls when
@@ -91,15 +107,9 @@ class _MyHomePageState extends State<MyHomePage> {
                 padding: EdgeInsets.all(16),
                 child: Text(
                   // If listening is active show the recognized words
-                  _speechToText.isListening
-                      ? '$_lastWords'
-                      // If listening isn't active but could be tell the user
-                      // how to start it, otherwise indicate that speech
-                      // recognition is not yet ready or not supported on
-                      // the target device
-                      : _speechEnabled
-                          ? 'Tap the microphone to start listening...'
-                          : 'Speech not available',
+                  !_isListening ? _lastWords : '',
+                  style: TextStyle(fontSize: 20.0),
+                  textAlign: TextAlign.center,
                 ),
               ),
             ),
@@ -107,11 +117,110 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed:
-            // If not yet listening for speech start, otherwise stop
-            _speechToText.isNotListening ? _startListening : _stopListening,
-        tooltip: 'Listen',
-        child: Icon(_speechToText.isNotListening ? Icons.mic_off : Icons.mic),
+        onPressed: _isListening ? _stopListening : _startListening,
+        tooltip: _isListening ? 'Stop Listening' : 'Listen',
+        child: Icon(_isListening ? Icons.mic_off : Icons.mic),
+      ),
+    );
+  }
+}
+
+class ResultPage extends StatelessWidget {
+  final String lastWords;
+
+  const ResultPage({Key? key, required this.lastWords}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Result'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(16),
+            child: Text(
+              'Recognized words:',
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(16),
+              child: Text(
+                lastWords,
+                style: TextStyle(fontSize: 20.0),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Do something when Placeholder1 button is pressed
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.transparent,
+                      padding:
+                          EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+                    ),
+                    child: Text(
+                      'This is a placeholder button. It may contain longer text.',
+                      style: TextStyle(fontSize: 18),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                SizedBox(width: 20),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Do something when Placeholder2 button is pressed
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.transparent,
+                      padding:
+                          EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+                    ),
+                    child: Text(
+                      'This is a placeholder button. It may contain longer text.',
+                      style: TextStyle(fontSize: 18),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                SizedBox(width: 20),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Do something when Placeholder3 button is pressed
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.transparent,
+                      padding:
+                          EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+                    ),
+                    child: Text(
+                      'This is a placeholder button. It may contain longer text.',
+                      style: TextStyle(fontSize: 18),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 20),
+        ],
       ),
     );
   }
